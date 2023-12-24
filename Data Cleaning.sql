642f0761659b623bd967f99ea70ce88d02f28178
SorathF <154694595+SorathF@users.noreply.github.com>
2023-12-24 18:03:57 +0500

    Update and rename SQL code for data cleaning to Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,4 +1,4 @@
-Step 1: Check for duplicate rows
+--- Step 1: Check for duplicate rows
 
 SELECT ride_id, started_at, ended_at, 
 COUNT(*)
@@ -6,7 +6,7 @@ FROM cyclistic_data
 GROUP BY ride_id, started_at, ended_at
 HAVING COUNT(*) > 1;
 
-Step 2: Check for NULL values
+--- Step 2: Check for NULL values
 SELECT 
     COUNT(*) AS total_rows,
     COUNT(ride_id) AS ride_id_count,
@@ -24,14 +24,14 @@ SELECT
     COUNT(member_casual) AS member_casual_count
 FROM cyclistic_data;
 
-Step 3: Check for inconsistencies in rideable_type and member_casual columns
+--- Step 3: Check for inconsistencies in rideable_type and member_casual columns
 SELECT DISTINCT rideable_type
 FROM cyclistic_data;
 
 SELECT DISTINCT member_casual
 FROM cyclistic_data;
 
-Step 4: Creation of new table
+--- Step 4: Creation of new table
 Create Table cyclistic_data_cleaned AS
 (
 Select
@@ -50,7 +50,7 @@ WHERE
 (start_station_name IS NOT NULL AND end_station_name IS NOT NULL)
 );
 
-Step 5: Check for any outliers exist in Trip_Duration column
+--- Step 5: Check for any outliers exist in Trip_Duration column
 SELECT
 *
 FROM cyclistic_data_cleaned
